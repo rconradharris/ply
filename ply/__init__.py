50aacb6b96c97f5786f0e7505c47f10e51bb7748
Rick Harris <rconradharris@gmail.com>
2012-11-16 18:11:22 -0600

    TypofixPatch: /Users/rconradharris/Documents/code/ply_sandbox/ply/sandbox/patch-repo/Typofix.patch

--- ply/__init__.py
+++ ply/__init__.py
@@ -3,61 +3,90 @@ import os
 from ply import git
 
 
-class Ply(object):
-    def __init__(self):
-        self.working_repo = git.Repo('sandbox/working-repo')
-        self.patch_repo = git.Repo('sandbox/patch-repo')
+class Repo(object):
+    def __init__(self, path):
+        self.git_repo = git.Repo(path)
 
-    def save(self, since, quiet=False, reset=True):
-        """save takes a range of commits in the working-repo, creates patch files
-        from them, commits those patch files into the patch-repo, and then removes
-        the commits from the working repo.
-        """
-        # Create patch files
-        orig_filenames = self.working_repo.format_patch(since)
+    @property
+    def path(self):
+        return self.git_repo.path
 
-        if not orig_filenames:
+
+class WorkingRepo(Repo):
+    """Represents our local fork of the upstream repository.
+
+    This is where we will create new patches (save) or apply previous patches
+    to create a new patch-branch (restore).
+    """
+    def format_patches(self, since):
+        """Create patch files since a given commmit."""
+        filenames = self.git_repo.format_patch(since)
+
+        if not filenames:
             raise Exception('no patch files generated')
 
-        new_filenames = []
+        # Remove number prefix from patch filename since we use a `series`
+        # file (like quilt) to order the patches
+        patch_paths = []
+        for filename in filenames:
+            orig_path = os.path.join(self.path, filename)
+            new_filename = filename.split('-', 1)[1]
+            new_path = os.path.join(self.path, new_filename)
+            os.rename(orig_path, new_path)
 
-        # Move patch files into patch repo and add to series file
-        with open(os.path.join(self.patch_repo.path, 'series'), 'a') as f:
-            for orig_filename in orig_filenames:
-                orig_path = os.path.join(self.working_repo.path, orig_filename)
+            patch_paths.append(new_path)
 
-                # Remove number prefix from patch filename since we use a `series`
-                # file (like quilt) to order the patches
-                new_filename = orig_filename.split('-', 1)[1]
-                new_path = os.path.join(self.patch_repo.path, new_filename)
+        return patch_paths
 
-                # Dedup any identically named patch files using a suffix
-                if os.path.exists(new_path):
-                    name, ext = new_filename.rsplit('.', 1)
+
+class PatchRepo(Repo):
+    """Represents a git repo containing versioned patch files."""
+
+    def add_patches(self, patch_paths, quiet=True):
+        """Adds and commits a set of patches into the patch repo."""
+        with open(os.path.join(self.path, 'series'), 'a') as f:
+            for orig_patch_path in patch_paths:
+                filename = os.path.basename(orig_patch_path)
+
+                patch_path = os.path.join(self.path, filename)
+                if os.path.exists(patch_path):
+                    name, ext = patch_path.rsplit('.', 1)
                     for dedup in xrange(999):
-                        new_filename = "%s-%d.%s" % (name, dedup + 1, ext)
-                        new_path = os.path.join(self.patch_repo.path, new_filename)
-                        if not os.path.exists(new_path):
+                        filename = "%s-%d.%s" % (name, dedup + 1, ext)
+                        patch_path = os.path.join(self.path, filename)
+                        if not os.path.exists(patch_path):
                             break
 
-                os.rename(orig_path, new_path)
-                f.write('%s\n' % new_filename)
-                new_filenames.append(new_filename)
+                os.rename(orig_patch_path, patch_path)
+                self.git_repo.add(filename)
+                f.write('%s\n' % filename)
 
-        # Commit patch files into patch repo
-        for filename in new_filenames:
-            self.patch_repo.add(filename)
+        self.git_repo.add('series')
 
-        self.patch_repo.add('series')
+        # TODO: improve this commit msg, for 1 or 2 patches use short form of
+        # just comma separated, for more than that, use long-form of number of
+        # patches one first-line and filenames enumerated in the body of
+        # commit msg.
+        self.git_repo.commit('Adding patches', quiet=quiet)
 
-        # TODO: improve this commit msg, for 1 or 2 patches use short form of just
-        # comma separated, for more than that, use long-form of number of patches
-        # one first-line and filenames enumerated in the body of commit msg.
-        self.patch_repo.commit('Adding patches', quiet=quiet)
 
-        # Rollback patches from working-repo
-        if reset:
-            self.working_repo.reset(since, hard=True, quiet=quiet)
+class Ply(object):
+    def __init__(self):
+        self.working_repo = WorkingRepo('sandbox/working-repo')
+        self.patch_repo = PatchRepo('sandbox/patch-repo')
+
+    def save(self, since, quiet=True):
+        """Saves a range of commits into the patch-repo.
+
+        1. Create the patches (using `git format-patch`)
+        2. Move the patches into the patch-repo (handling any dups)
+        3. Update the `series` file in the patch-repo
+        4. Commit the new patches
+        """
+        patch_paths = self.working_repo.format_patches(since)
+        self.patch_repo.add_patches(patch_paths, quiet=quiet)
+
 
 if __name__ == "__main__":
-    save('HEAD^1')
+    ply = Ply()
+    ply.save('HEAD^1', quiet=True)
